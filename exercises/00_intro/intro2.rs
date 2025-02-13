dc0c4c530455e008b9e52b7700d0a8da788b6cd5
Vincent <159125909+VinEckSie@users.noreply.github.com>
2025-02-13 16:04:17 +0000

    update: rustdoclang exercises

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -1,4 +1,9 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    println!("Hello world!");
+    //println!("Hello world!");
+    let condition = false;
+    let number = if condition {"ee"} else {""};
+
+
+    println!("number is {}",number);
 }
