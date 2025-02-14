9ebf5cb6aed3cfaf4ad51b30c5b3bc1798286a19
Vincent <159125909+VinEckSie@users.noreply.github.com>
2025-02-14 10:06:07 +0000

    update: if exes

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -1,9 +1,28 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
     //println!("Hello world!");
-    let condition = false;
-    let number = if condition {"ee"} else {""};
+   
+   let mut counter = 0;
+
+   let result = loop {
+        counter += 1;
+
+        let result2 = 'loop2: loop{
+            if counter  == 2{
+                break 'loop2 counter ;
+            }
+
+            counter += 1; 
+        };
+
+        println!("Inner loop returned: {}", result2);
 
 
-    println!("number is {}",number);
+        if counter == 5{
+            break counter + 10;
+        }
+   };
+
+   println!("result is {result}");
+
 }
