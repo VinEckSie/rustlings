b655f6812e1f80123fa4b8095072d0bec8418e49
Vincent <159125909+VinEckSie@users.noreply.github.com>
2025-02-21 09:59:43 +0000

    update: if exs

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -1,17 +1,6 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    //println!("Hello world!");
-    let tab = [3,4,33,33,22,55];
-    let mut index = 0;
+    println!("Hello world!");
 
-
-    while index < tab.len() {
-        println!("index : {0}, value : {1}", index, tab[index]);
-
-        index += 1;
-    }
- 
     
-
-
 }
