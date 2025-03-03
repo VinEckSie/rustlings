305030b9093116ea1f271ce8b5feee0124b4aa90
Vincent <159125909+VinEckSie@users.noreply.github.com>
2025-03-03 14:08:03 +0000

    update : primitives

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -2,5 +2,9 @@ fn main() {
     // TODO: Fix the code to print "Hello world!".
     println!("Hello world!");
 
-    
+   let cha = 'e';
+   let chaa: char = 'E';
+
+   println!("{} {} ", cha, chaa);
+
 }
