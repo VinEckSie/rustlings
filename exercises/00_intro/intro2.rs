8031b10c28978dc318a28403711a347686d6b9b2
Vincent <159125909+VinEckSie@users.noreply.github.com>
2025-03-04 15:19:42 +0000

    Update: primitives exs

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -2,9 +2,9 @@ fn main() {
     // TODO: Fix the code to print "Hello world!".
     println!("Hello world!");
 
-   let cha = 'e';
-   let chaa: char = 'E';
+    let services: (&'static str ,&'static str ,&'static str ,&'static str ) = ("invest","invest consulting","blockchain consulting","ai consulting");
+    let (serv1,_serv2,serv3,_serv4) = services;
 
-   println!("{} {} ", cha, chaa);
+    println!("{} {} {} {}", serv1, services.1, serv3, services.3);
 
 }
