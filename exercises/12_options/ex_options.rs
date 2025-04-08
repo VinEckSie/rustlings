c2c6f750aac041e3d3e90e8606879bd6eb5e3204
Vincent EckSie <new.riverside751@passmail.net>
2025-04-08 19:03:02 +0200

    feat(rustlings): Exercise options returning, mapping, combining, iterating values

--- exercises/12_options/ex_options.rs
+++ exercises/12_options/ex_options.rs
@@ -1,20 +1,21 @@
 use std::sync::RwLockReadGuard;
 
 fn main() {
-    //Nullable pointer
+    println!("\n\nNullable pointer: ");
     let nullable = None;
     check_option(nullable);
     
     let value = Some(Box::new(20));
     check_option(value);
-
     let question_value = None;
-    println!("\n? Operator: ");
+
+
+    println!("\n\n? operator: ");
     println!("Value is {:?}", get_value(question_value));
 
 
-    println!("\n? Adapters to work with references: ");
-    println!("as_ref: "); //used to avoid moving the Option<String>, so we can still use user later.
+    println!("\n\nAdapters to work with references: ");
+    println!("\nas_ref: "); //used to avoid moving the Option<String>, so we can still use user later.
     let mut user_id: Option<String> = Some("89859574973".to_string());
     //as_ref
     match user_id.as_ref() {
@@ -23,7 +24,7 @@ fn main() {
     }
     println!("User ID Confirmation: {:?}", user_id); // name is still usable because we didn't take ownership
 
-    println!("as_mut: "); //lets you change the value inside the Option directly.
+    println!("\nas_mut: "); //lets you change the value inside the Option directly.
     match user_id.as_mut() {
         Some(user_id) => {
             *user_id = "new8787878787".to_string();
@@ -33,62 +34,80 @@ fn main() {
     }
     println!("User ID Confirmation: {:?}", user_id);
 
-    println!("as_deref: "); // rust issue stand by
+    println!("\nas_deref: "); // rust issue stand by #87121
+    let user_mail: Option<Box<String>> = Some(Box::new("mail@example.com".to_string()));
+    let string_slice = user_mail.as_deref();
 
-    println!("as_deref_mut: "); // editing values inside smart pointers like String, Box<T>, etc.
+    match string_slice.as_deref().map(|ref_to_string| ref_to_string.as_str()) {
+        Some("mail@example.com") => println!("Matched!"),
+        Some(other) => println!("Other: {}", other),
+        None => println!("None"),
+    }
+
+    println!("\nas_deref_mut: "); // editing values inside smart pointers like String, Box<T>, etc.
     let mut boxed_name = Box::new(Some("locust".to_string()));
     clear_user_id(&mut boxed_name);
 
     //as pin ref() used inside custom future/stream types or low-level async code. Not needed in everyday Rust.
     //as pin mut() Used when building custom futures, generators, or safely mutating data that must not move
-    
-    println!("Extracting contained value: ");
-    // expect("msg")	    ✅ Panic	custom error "msg"	Must-have value, want clear error
+
+
+    println!("\n\nExtracting contained value: ");
+    println!("\nexpect: "); //✅ Panic	custom error "msg"	Must-have value, want clear error
     let my_id_custom: Option<i32> = Some(-76767676);
     //let my_id_custom: Option<i32> = None;
-
     // my_id_custom.expect("wallet_id not found");
-    // unwrap()	            ✅ Panic	generic error message	Quick debug code / tests only | Avoid in Production
+
+    println!("\nunwrap: "); //✅ Panic	generic error message	Quick debug code / tests only | Avoid in Production
     //my_id_custom.unwrap();
-    // unwrap_or(x)	        ❌ No panic	Very common in apps, CLI, configs.
-    //my_id_custom.unwrap_or(00000000);
-    // unwrap_or_default()	❌ No panic	Great for configs, deserialization, init logic.
-    //my_id_custom.unwrap_or_default();
-    // unwrap_or_else(f)	❌ No panic	Most useful in performance-sensitive or async cases | Expensive or lazy fallback
-    // my_id_custom.unwrap_or_else(|| wallet_default_error());
+
+    println!("\nunwrap_or: "); //❌ No panic	Very common in apps, CLI, configs.
+    my_id_custom.unwrap_or(00000000);
+
+    println!("\nunwrap_or_default: "); //❌ No panic	Great for configs, deserialization, init logic.
+    my_id_custom.unwrap_or_default();
+
+    println!("\nunwrap_or_else: "); //❌ No panic	Most useful in performance-sensitive or async cases | Expensive or lazy fallback
+     my_id_custom.unwrap_or_else(|| wallet_default_error());
     println!("my_id_custom: {:?}", my_id_custom);
 
 
-    println!("\n? Transforming contained values: ");
+    println!("\n\nTransforming contained values: ");
     println!("Transform Option to Result: ");
-    // Use .ok_or(...) when you want to convert to Result with a simple error
+
+    println!("\nok_or: ");//when you want to convert to Result with a simple error
     let mut id_result: Result<_, &str> = my_id_custom.ok_or("Error: No id result");
-    // Use .ok_or_else(...) when the error needs logic or is expensive
+    println!("id_result: {:?}", id_result);
+
+    println!("\nok_or_else: ");//when the error needs logic or is expensive
     id_result = my_id_custom.ok_or_else(|| result_default_error());
-    // Use .transpose() when you have an Option<Result> and want clean error handling
+    println!("id_result: {:?}", id_result);
+
+    println!("\ntranspose: ");//when you have an Option<Result> and want clean error handling
     let maybe_id: Option<Result<_,_>> = Some(id_result);
     let id_verified: Result<_,_> = maybe_id.transpose();
     println!("id_result: {:?}", id_result);
     println!("id_verified: {:?}", id_verified);
     
-    println!("Transform Some variant: ");
+    println!("\nTransform Some variant: ");
     println!("Filter: keep Some(t) only if it passes a test: ");
-    let my_megative_id = my_id_custom.filter(|var| var.is_negative()); 
-    
+    let my_megative_id = my_id_custom.filter(|var| var.is_negative());
     if let Some(my_megative_id) = my_megative_id {
         println!("id is negative: {:?}", my_megative_id);
     }
     
-    println!("Flatten: chaining optional logic that might return another Option:");
+    println!("\nFlatten: chaining optional logic that might return another Option:");
     println!("Flatten, parsing value: ");
     let account_id = Some("588");
     let parsed =  account_id.map(|acc| acc.parse::<u32>().ok()).flatten();
     println!("account_id parsed: {:?}", parsed);
-    println!("Flatten, error handling: ");
+
+    println!("\nFlatten, error handling: ");
     let account_error: Result<Result<i32, &str>, &str> = Ok(Ok(642));
     let account_error_flat = account_error.iter().flatten();
     println!("Flatten, flat error: {:?}", account_error_flat);
-    println!("Flatten, working with iterators: ");
+
+    println!("\nFlatten, working with iterators: ");
     let my_vec = vec![Some("588"), None, Some("554")];
     println!("my_vec_processed: {:?}", my_vec
             .iter()
@@ -96,13 +115,88 @@ fn main() {
             .collect::<Vec<_>>());
     
         
-    println!("Return a value (not an option) : transform Option<T> to a value: ");
-    //map or Use case: You want to always get a value, even if it's None.
-    //map or else Use case: Computing the fallback is expensive or contextual.
+    println!("\n\nReturn a value (not an option): ");
+    println!("\nmap: "); //apply a function to the option value, need to add unwrap to access the value
+    let mut liquidation_threshold = Some(0.75);
+    let liquidation_threshold_per = liquidation_threshold.map(|lt| lt * 100.0);
+    println!("liquidation in %: {}", liquidation_threshold_per.as_ref().unwrap());
+
+    println!("\nmap_or: "); //You want to always get a value, even if it's None, give the inner value, no need unwrap
+    liquidation_threshold = None;
+    let liquidation_threshold_dft = liquidation_threshold.map_or(50.0, |lt| lt * 100.0);
+    println!("liquidation (default value) {}", liquidation_threshold_dft);
+
+    println!("\nmap_or_else: ");//map or else Use case: Computing the fallback is expensive or contextual.
+    let liquidation_threshold_exp = liquidation_threshold.map_or_else (|| map_default(), |lt| lt * 100.0);
+    println!("liquidation (default expensive value) {}", liquidation_threshold_exp);
+
+    println!("\n\nCombine two options: ");
+
+    println!("\nzip: ");//zip Use case: Combine two options into a tuple only if both exist.
+    let liquidation_zipped = my_megative_id.zip(parsed);
+    println!("liquidation zipped: {liquidation_zipped:?}");
+
+    println!("\nzip with: ");//zip with Use case: Combine two Options with logic (e.g., add, concat, etc.)
+    println!("WARNING: works only in nighly rust");
+    let liquidation_zipped_with = my_megative_id.zip(Some(-35)).map(|(a,b)| a + b);
+    println!("liquidation zipped: {liquidation_zipped_with:?}");
+
+    println!("\n\nBool operator: ");
+    println!("\nand_then: "); // chaining multiple operations like parsing or validation that might return Option without match/unwrap.
+    println!("or (STATIC feedback): ");
+    // static fallback when you have a backup plan, easy way to say “use this if missing”.
+    //  let network_name = Some("SOLANA");
+    let network_name: Option<&str> = None;
+    let network_updated = network_name
+        .and_then(|n| if n.contains("SOLANA") {Some(n)} else {None})
+        .or(Some("Polygon"));
+    println!("network_updated with or: {:?}", network_updated);
+
+    println!("\nand_then: "); // chaining multiple operations like parsing or validation that might return Option without match/unwrap.
+    println!("or_else (LAZY feedback): "); // Use or_else to call a function only if None—a lazy fallback that only computes if needed,
+    let network_updated_else = network_name
+        .and_then(|n| if n.contains("SOLANA") {Some(n)} else {None})
+        .or_else(|| or_else_function(network_name));
+    println!("network_updated with or_else: {:?}", network_updated_else);
     
-    println!("Combine two options: ");
-    //zip Use case: Combine two options into a tuple only if both exist.
-    //zip with Use case: Combine two Options with logic (e.g., add, concat, etc.)
+    //NOT USED OFTEN
+    //and(opt) = Too limited — and_then() is always more flexible (can return different type, apply logic)
+    // xor(opt) = Rarely needed — only useful in weird “only one must be Some” situations, almost never seen in real code.
+    // and_then(None → None) & or_else(Some → ignored) : These are already covered by and_then() and or_else() — no need to remember the internals.
+
+
+    println!("\n\nIterate: ");
+    println!("\ninto_iter: ");
+    // You want to move out the value when you do not need anymore, turning it into an iterator of 0 or 1 item.
+    let protocol_name: Option<&str> = Some("uniswap");
+    println!("protocol_name: {:?}", protocol_name); //here is consumed
+    
+
+    println!("\niter (more often used with vec, hashmap,...): "); // You want to read the value, Borrows the value inside the Option as &T
+    println!("iter_mut (more often used with vec, hashmap,..): "); // You want to modify the value inside the Option safely, Borrows the value inside as &mut T.
+
+
+    println!("\n\nCollecting infos (not very useful except for writing generic code): "); //collecting into (if any is None => None is returned)
+    println!("collect: "); // when you're transforming or validating data and want to stop on first failure. when you want all-or-nothing collection
+    println!("sum: "); // when you're aggregating validated inputs (e.g. user scores, parsed data). when You want to add only if all are valid
+    println!("product: "); // when you’re computing totals from optional or fallible data sources. when You want to multiply only if all valid
+
+
+    println!("\n\nModifying in place: ");
+    println!("\ninsert: ");
+    //you want to force a new value
+
+    println!("\nget or insert: ");
+    //you want to ensure a value is present
+
+    println!("\nget or insert with: ");
+    //when value is expensive to compute
+
+    println!("\ntake: ");
+    //you want to move out the value
+
+    println!("\nreplace: ");
+    //you want to swap the value cleanly
 }
 
 //Nullable pointer
@@ -150,51 +244,13 @@ fn result_default_error() -> &'static str {
     "wallet_default_error"
 }
 
+//ok_or_else
+fn map_default() -> f64 {
+    25.0
+}
 
-//bool operator
-// and_then:
-    // chaining multiple operations like parsing or validation that might return Option without match/unwrap.
-// or_else (LAZY FEEDBACK)
-    // Use or_else to call a function only if None—a lazy fallback that only computes if needed,
-    // useful when the fallback requires logic, and you don't want to run heavy code unnecessarily.
-// or (STATIC FEEDBACK)
-    // static fallback when you have a backup plan, easy way to say “use this if missing”.
+fn or_else_function(network: Option<&str>) -> Option<&str> {
+    Some(network.as_ref()).and(Some(" and Ethereum"))
+}
 
-//NOT USED OFTEN
-//and(opt) = Too limited — and_then() is always more flexible (can return different type, apply logic)
-// xor(opt) = Rarely needed — only useful in weird “only one must be Some” situations, almost never seen in real code.
-// and_then(None → None) & or_else(Some → ignored) : These are already covered by and_then() and or_else() — no need to remember the internals.
-
-
-//iterate
-// into_iter
-    // You want to move out the value when you do not need anymore, turning it into an iterator of 0 or 1 item.
-// iter
-    // You want to read the value, Borrows the value inside the Option as &T
-// iter_mut
-    // You want to modify the value inside the Option safely, Borrows the value inside as &mut T.
-
-
-//collecting into (if any is None => None is returned)
-//collect
-    // when you're transforming or validating data and want to stop on first failure.
-    // when you want all-or-nothing collection
-//sum
-    // when you're aggregating validated inputs (e.g. user scores, parsed data).
-    // when You want to add only if all are valid
-//product
-    // when you’re computing totals from optional or fallible data sources.
-    // when You want to multiply only if all valid
-
-//modifying in place
-//insert
-    //you want to force a new value
-//get or insert
-    //you want to ensure a value is present
-//get or insert with
-    //when value is expensive to compute
-//take
-    //you want to move out the value
-//replace
-    //you want to swap the value cleanly
 
