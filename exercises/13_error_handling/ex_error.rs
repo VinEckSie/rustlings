88c3ea1d95db74ce79feb2512c1ed2bd27ac29f6
Vincent EckSie <new.riverside751@passmail.net>
2025-04-17 19:45:34 +0200

    feat(rustlings): Practicing Struct Generic Methods

--- exercises/13_error_handling/ex_error.rs
+++ exercises/13_error_handling/ex_error.rs
@@ -43,7 +43,10 @@ fn main() {
     let char = last_char_of_first_line(file.unwrap().as_str());
     println!("Character: {:?}", char);
 
+    
     //generics
+    //monomorphization; process of turning generic code to specific code
+    
     //function
     let number_list = vec![34, 50, 125, 100, 65];
     let result = largest(&number_list);
@@ -58,24 +61,23 @@ fn main() {
     let coords_3 = PointSimpleGeneric { x: 5.0, y: 10.0 };
     // let coords_2 = PointSimpleGeneric { x: 2, y: 3.0 }; //KO because this struct only afford 1 type as generics
     
-    let coords4 = PointDoubleGeneric { x: 5.0, y: 4.2 };
-    let coords5 = PointDoubleGeneric { x: 5, y: 5.0 }; //OKs because this is a double generic struct
+    let coords4 = PointDoubleGeneric { x: 5.0, y: 4 };
+    let coords5 = PointDoubleGeneric { x: 3, y: 2.0 }; //OKs because this is a double generic struct
     println!("If you’re finding you need lots of generic types in your code, \nIt could indicate that your code needs restructuring into smaller pieces.");
+    println!("DoubleGeneric, get_x(): {}", coords5.get_x());
     
-    //enum
-    //option and result example
-
-    //method
-    //implememt method x on struct Point
-    //implement method only for a certain type
-    //example with mixup
-
-    //monomorphization; explain the concept
-
-    //result
-    //use of parse with number litteral
-
+    //generic methods from generic struct
+    let coords_u32 = PointSimpleGeneric{x: 65, y: 560};
+    let coords_u8 = PointSimpleGeneric{x: -1, y: -1};
+    println!("coords_u32 y : {}",coords_u32.get_y());
+    println!("coords_u32 x : {}",coords_u32.get_x());
+     // let y_u8 = coords_u8.get_y();//not working, because get_y() implemented only for i32 type
+    
+    let new_double_coords = coords4.mix(coords5);
+    println!("new double coords: {:#?}", new_double_coords)
+    
     //boxing error greate for smalll projects or prototyping
+    
 }
 
 //Generics part
@@ -103,26 +105,51 @@ struct PointSimpleGeneric<T> {
     y: T,
 }
 
+#[derive(Debug)]
 struct PointDoubleGeneric<T,U> {
     x: T,
     y: U,
 }
 
-impl<T, U> PointSimpleGeneric<T>{
-    fn new(x: T, y: U) -> Self {
-        PointSimpleGeneric{x, y}
+//filter a simple geenric on a tyep
+impl PointSimpleGeneric<u32>{
+    fn get_y(&self) -> &u32{
+        &self.y
     }
-    
+}
+
+impl<T> PointSimpleGeneric<T>{
     fn get_x(&self) -> &T{
         &self.x
     }
 }
 
-impl<T> PointSimpleGeneric<u32>{
-    fn get_y(&self) -> &u32{
+//implement a constructor and a get on a double generic
+impl<T, U> PointDoubleGeneric<T, U>{
+    fn new(x: T, y: U) -> Self {
+        PointDoubleGeneric{x, y}
+    }
+    
+    fn get_x(&self) -> &T{
+        &self.x
+    }
+    
+    //mix up generic from the struct origin with a new point with other generics
+    fn mix<T1, U1>(self, other_point: PointDoubleGeneric<T1, U1>) -> PointDoubleGeneric<T,U1> {
+        PointDoubleGeneric{
+            x: self.x,
+            y: other_point.y,
+        }
+    }
+}
+
+//filter on one type for the double generic
+impl<T> PointDoubleGeneric<T, f64>{
+    fn get_y(&self) -> &f64{
         &self.y
     }
 }
+
 
 
 
