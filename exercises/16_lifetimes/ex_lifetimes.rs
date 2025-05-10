2dfd51c3942192bbbc7b839e599e35548c90f6f1
VinEckSie <new.riverside751@passmail.net>
2025-05-10 20:28:10 +0200

    feat(rustlings) learning lifetimes

---
+++ exercises/16_lifetimes/ex_lifetimes.rs
@@ -0,0 +1,15 @@
+// Lifetimes are annotated below with lines denoting the creation
+// and destruction of each variable.
+// `i` has the longest lifetime because its scope entirely encloses 
+// both `borrow1` and `borrow2`. The duration of `borrow1` compared 
+// to `borrow2` is irrelevant since they are disjoint.
+fn main() {
+    let r;
+
+    {
+        let x = 5;
+        r = &x;
+    }
+
+    println!("r: {r}");
+}  
