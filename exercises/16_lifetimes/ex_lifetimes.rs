b88e7088d1d9d73f4e2ffee7ea03ddb56cb6e995
VinEckSie <new.riverside751@passmail.net>
2025-05-12 19:08:41 +0200

    Learning lifetimes

--- exercises/16_lifetimes/ex_lifetimes.rs
+++ exercises/16_lifetimes/ex_lifetimes.rs
@@ -1,15 +1,42 @@
 // Lifetimes are annotated below with lines denoting the creation
 // and destruction of each variable.
-// `i` has the longest lifetime because its scope entirely encloses 
-// both `borrow1` and `borrow2`. The duration of `borrow1` compared 
+// `i` has the longest lifetime because its scope entirely encloses
+// both `borrow1` and `borrow2`. The duration of `borrow1` compared
 // to `borrow2` is irrelevant since they are disjoint.
 fn main() {
-    let r;
+    let word1 = String::from("loooong_chain_here");
+    let result;
 
     {
-        let x = 5;
-        r = &x;
+        let word2 = String::from("short_chain_here");
+        result = longest(word1.as_str(), word2.as_str());
+        println!("{result:?}");
     }
 
-    println!("r: {r}");
-}  
+    //println!("{result}"); KO if word2 is of String type because of lifetime rule in longest function signature
+
+    let text = String::from(" All humans are born equals. Just some work harder.");
+    let quote = text.split(".").nth(1).unwrap_or_else(|| "no quote");
+    let my_note = Notes { quote };
+
+    println!("{quote}");
+}
+
+fn longest<'a>(first_num: &'a str, second_num: &'a str) -> &'a str {
+    if first_num.len() > second_num.len() {
+        first_num
+    } else {
+        second_num
+    }
+}
+
+//if the return type has a lifetime, one of the parameters must have too otherwise dangling reference about the local var returned
+// fn longest_local<'a>(first_num: &str, second_num: &str) -> &'a str {
+//     let result = String::from("dangling reference");
+//     result.as_str()
+// }
+
+//struct def
+struct Notes<'a> {
+    quote: &'a str,
+}
